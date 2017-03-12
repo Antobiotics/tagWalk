8e67bf7d3395a0beeb14302ef5934d8859d17832
Gregoire Lejay <gregoire.lejay@gmail.com>
2017-03-12 22:46:57 +0000

    Pagination

--- scrape.py
+++ scrape.py
@@ -5,12 +5,16 @@ import urllib2
 from bs4 import BeautifulSoup
 
 OUTPUT_DIR = './data'
+PICS_DIR = OUTPUT_DIR + '/images'
 TAG_PATH = '/'.join([OUTPUT_DIR, 'tags.csv'])
 BASE_URL = 'https://www.tag-walk.com/en/'
 TAG_BASE = r'/en/photo/list/woman/all-categories/all-cities/all-seasons/all-designers/(.*)$'
 TAG_REGEX = re.compile(TAG_BASE)
 
+BASE_PHOTOS = 'https://www.tag-walk.com/en/photo/list/woman/all-categories/all-cities/all-seasons/all-designers/'
 
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
 
 def find_tags():
     print "Searching for tags in %s" %(BASE_URL)
@@ -46,4 +50,46 @@ def get_tags(path=TAG_PATH):
     return read_tags(path=path)
 
 tags = get_tags()
-print tags
+
+
+def get_tag_num_results(tag):
+    url_format = BASE_PHOTOS + tag + '?page=1'
+    page = urllib2.urlopen(url_format).read()
+    soup = BeautifulSoup(page, "lxml")
+    soup.prettify()
+    div = soup.find('div', {"class": "nbresult"})
+    nb = div.text.replace(' ', '').replace('Results', '')
+    return int(nb)
+
+for tag in tags:
+    print tag
+    tag_path ='/'.join([PICS_DIR, tag])
+
+    if not os.path.exists(tag_path):
+        os.makedirs(tag_path)
+
+    nb_results = get_tag_num_results(tag)
+
+    img_counter = 0
+    page_counter = 1
+
+    while img_counter <= nb_results:
+        url_format = BASE_PHOTOS + tag + '?page=%s' %(page_counter)
+        print "Fetching %s" %(url_format)
+        page = urllib2.urlopen(url_format).read()
+        soup = BeautifulSoup(page, "lxml")
+        soup.prettify()
+
+        anchors = soup.findAll('div', {"class": "photoimg"})
+        for anchor in anchors:
+            img_name = anchor.a.img['alt']
+            img_src = anchor.a.img['src']
+            path ='/'.join([tag_path, img_name])
+            print path
+            with open(path, 'w') as img_file:
+                req = urllib2.Request(img_src, headers=headers)
+                img = urllib2.urlopen(req).read()
+                img_file.write(img)
+                img_counter = img_counter + 1
+
+        page_counter = page_counter + 1
