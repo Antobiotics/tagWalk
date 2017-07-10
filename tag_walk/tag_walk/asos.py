364d93b23f5eb81d4810320cbf21ed8ba425addb
Gregoire Lejay <gregoire.lejay@gmail.com>
2017-07-10 14:34:39 +0100

    :nail_care: Clean up iids and sgids from urls for easier joins

--- tag_walk/tag_walk/asos.py
+++ tag_walk/tag_walk/asos.py
@@ -5,13 +5,32 @@ import tag_walk.logger as l
 
 from tag_walk.postgres import AsosConnection
 
-IID_CLEAN_REGEEX = re.compile(r'(.*)&.*$')
+IID_CLEAN = re.compile(r'(.*)&.*$')
+URL_DETAIL_GET_PROD = re.compile(r'.*/(.*)\?iid=(.*)&.*')
+URL_DETAIL_GET_GROUP = re.compile(r'.*/(.*)\?sgid=(.*)&.*')
+SGID_EXTRACT = re.compile(r'.*&sgid=(.*)')
+
+def extract_sgid(x):
+    res = SGID_EXTRACT.search(x)
+    if res:
+        return res.group(1)
+    return None
+
+def extract_iid_from_url(x):
+    res = URL_DETAIL_GET_PROD.search(x)
+    if res:
+        return res.group(1)
+
+    res = URL_DETAIL_GET_GROUP.search(x)
+    if res:
+        return res.group(1)
+    return x
 
 def clean_brand_id(brand_id):
     return brand_id.replace('&via=top', '')
 
 def clean_iid(iid):
-    res = re.search(IID_CLEAN_REGEEX, iid)
+    res = re.search(IID_CLEAN, iid)
     if res:
         return res.group(1)
     return iid
@@ -50,10 +69,14 @@ class Asos(AsosConnection):
         df['price_orig'] = df['price_orig'].apply(clean_brackets)
         df['price_cur'] = df['price_cur'].apply(clean_brackets)
 
+        df['sgid'] = df['iid'].apply(extract_sgid)
+        df['iid'] = df['iid'].apply(clean_iid)
+
         return df.drop_duplicates()
 
     def get_item_details(self):
         df = self.get_table_as_pandas('public', 'item_details')
+        df['iid'] = df['url'].apply(extract_iid_from_url)
 
         df['cat1'] = df['cat1'].apply(clean_item_category)
 
