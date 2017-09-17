49fcf808382467ac7ada3524ae9483ad0e72e49b
gregoire lejay <gregoire.lejay@gmail.com>
2017-09-17 20:45:07 +0100

    :sparkles: s3 module

---
+++ tag_walk/fachung/s3.py
@@ -0,0 +1,9 @@
+from luigi.contrib.s3 import S3Client
+
+import fachung.configuration as conf
+
+def get_s3_client(self):
+    access_key = conf.get_config().get('s3', 'aws_access_key_id')
+    secret = conf.get_config().get('s3', 'aws_secret_access_key')
+
+    return S3Client(access_key, secret)
